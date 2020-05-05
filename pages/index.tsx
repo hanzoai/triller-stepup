192bd5a2bc0db9e7e725f21666fac27e3b311205
David Tai <david@verus.io>
2020-05-05 17:53:49 -0500

    no bottom thing

--- pages/index.tsx
+++ pages/index.tsx
@@ -258,7 +258,7 @@ export default () => {
           </Container>
         </div>
       </div>
-      <iframe src='https://triller.co' className={classes.iframe}/>
+      { /* <iframe src='https://triller.co' className={classes.iframe}/> */ }
     </div>
   )
 }
